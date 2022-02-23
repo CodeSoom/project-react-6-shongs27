823bbdcc7df885d0ae2a25e886803a17ca81fe12
shongs27 <shongs27@naver.com>
2022-02-24 01:37:01 +0900

    [Feat] 방문자 수 조사 페이지 단순 렌더링 구현

--- src/commons/aside/sections/AnalysticSection.jsx
+++ src/commons/aside/sections/AnalysticSection.jsx
@@ -1,7 +1,14 @@
 export default function AnalysticSection() {
   return (
     <div>
-      <h3>구글 애널리틱스</h3>
+      <h3>전체 방문자</h3>
+      <span>{'123,711 <test>'}</span>
+      <div className="today">
+        <span>오늘</span>
+      </div>
+      <div className="tomorrow">
+        <span>내일</span>
+      </div>
     </div>
   );
 }
