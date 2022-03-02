b49db297614ff0776d6837253c18bb58b593f4a3
shongs27 <shongs27@naver.com>
2022-03-02 17:27:11 +0900

    PR8 init

--- src/services/api.js
+++ src/services/api.js
@@ -1,39 +1,39 @@
 export async function fetchPageContents(category) {
-  const response = await fetch(`http://localhost:3000/${category}`);
+  const response = await fetch(`http://localhost:8000/${category}`);
   const data = await response.json();
 
   return data;
 }
 
 export async function fetchPageDetail(category, id) {
-  const response = await fetch(`http://localhost:3000/${category}/${id}`);
+  const response = await fetch(`http://localhost:8000/${category}/${id}`);
   const data = await response.json();
   return data;
 }
 
 export async function fetchRecentPosts() {
-  const response = await fetch('http://localhost:3000/recentPosts');
+  const response = await fetch('http://localhost:8000/recentPosts');
   const data = await response.json();
   return data;
 }
 
 export async function fetchPopularPosts() {
-  const response = await fetch('http://localhost:3000/popularPosts');
+  const response = await fetch('http://localhost:8000/popularPosts');
   const data = await response.json();
   return data;
 }
 
 export async function fetchSearchTarget(searchField) {
   //searchField를 어떻게 적용 시킬까?
-  const response = await fetch(`http://localhost:3000/search`);
+  const response = await fetch(`http://localhost:8000/search`);
   const data = await response.json();
   return data;
 }
 
 export async function postLogin(email, address) {
-  const response = await fetch('http://localhost:3000/login');
+  const response = await fetch('http://localhost:8000/login');
 
-  // const response = await fetch('http://localhost:3000/login', {
+  // const response = await fetch('http://localhost:8000/login', {
   //   method: 'POST',
   //   body: JSON.stringify({ email, address }),
   //   header: {
@@ -46,7 +46,7 @@ export async function postLogin(email, address) {
 }
 
 export async function postArticle(postForm) {
-  const response = await fetch('http://localhost:3000/post', {
+  const response = await fetch('http://localhost:8000/post', {
     method: 'POST',
     body: JSON.stringify(postForm),
     header: {
