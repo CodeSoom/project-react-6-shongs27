014e81077300439f1111b4f86990b1601dba8e6f
shongs27 <shongs27@naver.com>
2022-03-09 19:18:41 +0900

    배포 단계

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,16 +1,16 @@
 import MarkdownRender from './MarkdownRender';
 
+import styled from '@emotion/styled';
+const Container = styled.div({
+  width: '1000px',
+});
+
 export default function PostDetail({ postDetail }) {
   const { _id, writer, title, description, category, content, likes } =
     postDetail;
   return (
-    <>
-      <div>
-        <div>{_id}</div>
-        <div>{title}</div>
-        <div>{description}</div>
-      </div>
-      <MarkdownRender markdown={content} />
-    </>
+    <Container>
+      <MarkdownRender markdown={content} />;
+    </Container>
   );
 }
